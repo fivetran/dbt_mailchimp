09665db049346af5b95858d5698ffd18c185914c
fivetran-reneeli <renee.li@fivetran.com>
2023-07-28 22:58:45 +0200

    updates

--- models/intermediate/int_mailchimp__automation_activities_by_member.sql
+++ models/intermediate/int_mailchimp__automation_activities_by_member.sql
@@ -23,36 +23,40 @@ with activities as (
     select 
         member_id,
         list_id,
+        automation_email_id,
         sum(case when action_type = 'open' then 1 end) as opens,
         sum(case when action_type = 'click' then 1 end) as clicks, 
         count(distinct case when action_type = 'open' then member_id end) as unique_opens, 
         count(distinct case when action_type = 'click' then member_id end) as unique_clicks
     from activities
-    group by 1,2
+    group by 1,2,3
 
 ), sends as (
 
     select
         member_id,
         list_id,
+        automation_email_id,
         count(*) as sends
     from recipients
-    group by 1,2
+    group by 1,2,3
 
 ), unsubscribes_xf as (
 
     select
         member_id,
         list_id,
+        campaign_id,
         count(*) as unsubscribes
     from unsubscribes
-    group by 1,2
+    group by 1,2,3
 
 ), joined as (
 
     select
         coalesce(pivoted.member_id, sends.member_id, unsubscribes_xf.member_id) as member_id,
         pivoted.list_id,
+        pivoted.automation_email_id,
         pivoted.opens,
         pivoted.clicks,
         pivoted.unique_opens,

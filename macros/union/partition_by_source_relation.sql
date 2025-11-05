3b1e81675f54f7089971b5657d4e568a4e954f49
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2025-11-05 12:59:47 -0600

    fix macro

--- macros/union/partition_by_source_relation.sql
+++ macros/union/partition_by_source_relation.sql
@@ -1,6 +1,6 @@
 {%- macro partition_by_source_relation(has_other_partitions='yes', alias=None) -%}
 
-{{ adapter.dispatch('partition_by_source_relation', 'mailchimp') () }}
+{{ adapter.dispatch('partition_by_source_relation', 'mailchimp') (has_other_partitions, alias) }}
 
 {%- endmacro %}
 
